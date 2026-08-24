4985ce4c8c3fa70bb104eba72551fe5d2ee82d99
Restaurant app <zeelp9963@gmail.com>
2026-08-24 14:03:23 +0530

    Initialize Firebase and Firestore in firebase.js

---
+++ firebase.js
@@ -0,0 +1,64 @@
+// ================= FIREBASE =================
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+getFirestore,
+collection,
+getDocs,
+getDoc,
+setDoc,
+addDoc,
+doc,
+updateDoc,
+deleteDoc,
+onSnapshot,
+query,
+orderBy,
+serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+// ================= CONFIG =================
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyDi40oL-JtE9LXF8CPGFHHLL6Hqe4p_GDA",
+
+authDomain: "restaurant-63230.firebaseapp.com",
+
+projectId: "restaurant-63230",
+
+storageBucket: "restaurant-63230.firebasestorage.app",
+
+messagingSenderId: "624519670365",
+
+appId: "1:624519670365:web:f23d10079a9adf2c51bbb4"
+
+};
+
+// ================= INITIALIZE =================
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+// ================= GLOBAL =================
+
+window.db = db;
+
+window.fb = {
+
+collection,
+getDocs,
+getDoc,
+setDoc,
+addDoc,
+doc,
+updateDoc,
+deleteDoc,
+onSnapshot,
+query,
+orderBy,
+serverTimestamp
+
+};
